b8f8c37ed98a3ab320ed9b0d5e913765ffdb4a43
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-15 16:31:48 +0100

    added todos list to render in html template

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+# DB import
+from .models import Task
+
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+
+    context = {'tasks': tasks}
+    return render(request, 'tasks/list.html', context)
