4e499948ddfe28d5c7836441e09a426e9cf51cfa
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-15 17:22:14 +0100

    added chaanging endpoint to urls

--- todo_app/tasks/urls.py
+++ todo_app/tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='list'),
+    path('update_task/<str:pk>/',  views.update_task, name='update_task')
 ]
