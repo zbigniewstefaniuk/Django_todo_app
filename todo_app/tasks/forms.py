56cba33b4394217707392cc8eea4f5d24d9d9afe
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-15 21:54:43 +0100

    added custom css atributes to form field

--- todo_app/tasks/forms.py
+++ todo_app/tasks/forms.py
@@ -6,7 +6,14 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+    # adding custom css parameters to form field
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'todo-input',
+            'placeholder': 'Add new task...'
+        }))
     # making in html form with this class
+
     class Meta:
         model = Task
         fields = '__all__'
