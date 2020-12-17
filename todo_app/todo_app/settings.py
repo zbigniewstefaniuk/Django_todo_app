f8f41fa49c93647d3b3643c461144597e96b592d
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-17 19:53:29 +0100

    updated setting with heroku instrucitons

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
